7abcdf82e1e0523624d78dc2d6719d150f4213d1
heiyu@shelpe.com <heiyu@shelpe.com>
2017-04-03 23:12:22 +0800

    modify the files

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
+import Routes from './routes';
+import {hashHistory} from 'history';
 ReactDOM.render(
-    <h1>hello wordl</h1>,
+    <Router history={hashHistory} routes={Routes} />,
     document.getElementById('app')
 );
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
 
-const Routes = {
-    path: '/',
-    getIndexRoute(location,callback){
-        require.ensure([],function(require){
-            callback(null,require('../routes/Index/Index'))
-        })
-    },
-    getComponents(location,callback){
-        require.ensure([],function(require){
-            callback(null,require('../routes/About/Index'))
-        })
-    },
-    getChildRoutes(location,callback){
+export default {
+    component: 'div',
+    childRoutes: [
+        {
+            path: '/',
+            component: require('../routes/Index/Index'),
+            childRoutes: [
+                require('../routes/Index/Index'),
+                require('../routes/About/Index')
+            ]
+        }
+    ]
 
-    },
+    // getIndexRoute(location,callback){
+    //     require.ensure([],function(require){
+    //         callback(null,require('../routes/Index/Index'))
+    //     })
+    // },
+    // getComponents(location,callback){
+    //     require.ensure([],function(require){
+    //         callback(null,require('../routes/About/Index'))
+    //     })
+    // },
+    // getChildRoutes(location,callback){
+    //
+    // },
 }
-export const;
 
 
 // import Index from '../routes/Index/Index';
